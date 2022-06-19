d81378aef7fc9d7b576b3cdbd416a5996f797051
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2022-06-19 19:07:44 +0200

    FE mutations

--- client/src/DisplayData.jsx
+++ client/src/DisplayData.jsx
@@ -1,4 +1,4 @@
-import { gql, useLazyQuery, useQuery } from "@apollo/client";
+import { gql, useLazyQuery, useMutation, useQuery } from "@apollo/client";
 import React, { useState } from "react";
 
 const QUERY_ALL_USERS = gql`
@@ -41,22 +41,52 @@ const GET_MOVIE_BY_NAME = gql`
   }
 `;
 
+const CREATE_USER_MUTATION = gql`
+  mutation CreateUser($input: CreateUserInput!) {
+    createUser(input: $input) {
+      age
+      id
+      name
+      nationality
+      username
+    }
+  }
+`;
+
 const DisplayData = () => {
   const [movieSearched, setMovieSearched] = useState("");
+
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [age, setAge] = useState(0);
+  const [nationality, setNationality] = useState("");
 
   const { data, loading, error } = useQuery(QUERY_ALL_USERS);
   const { data: moviesData } = useQuery(QUERY_ALL_MOVIES);
 
+  const [createUser] = useMutation(CREATE_USER_MUTATION, { refetchQueries: [QUERY_ALL_USERS] });
+
   const [fetchMovie, { data: movieSearchedData, error: movieError }] = useLazyQuery(GET_MOVIE_BY_NAME);
 
-  console.log(movieSearchedData);
-
-  console.log(moviesData);
   if (loading) return <h1>Data is loading</h1>;
   if (error) console.log(error);
   if (movieError) console.log(movieError);
   return (
     <div>
+      <div>
+        <input type='text' placeholder='Name' onChange={(e) => setName(e.target.value)} />
+        <input type='text' placeholder='Username' onChange={(e) => setUsername(e.target.value)} />
+        <input type='number' placeholder='Age' onChange={(e) => setAge(Number(e.target.value))} />
+        <input
+          type='text'
+          placeholder='Nationality'
+          onChange={(e) => setNationality(e.target.value.toUpperCase())}
+        />
+        <button onClick={() => createUser({ variables: { input: { name, username, age, nationality } } })}>
+          Create User
+        </button>
+      </div>
+
       {data?.users &&
         data.users.map((user) => (
           <div>
@@ -72,9 +102,7 @@ const DisplayData = () => {
         <input
           type='text'
           placeholder='Interstellar...'
-          onChange={(event) => {
-            setMovieSearched(event.target.value);
-          }}
+          onChange={(event) => setMovieSearched(event.target.value)}
         />
         <button onClick={() => fetchMovie({ variables: { name: movieSearched } })}>Fetch Data</button>
         <div>{movieSearchedData && <h1>{movieSearchedData.movie.name}</h1>}</div>
